12d86d0f44cb263205279e5d5d3431916ee5463d
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-05-25 16:19:21 -0700

    Update concat_source_headers_and_definitions.sh

--- game_engine_related/source/concat_source_headers_and_definitions.sh
+++ game_engine_related/source/concat_source_headers_and_definitions.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+
+#headers (non defined variables and function "references")
+cat "./start_game_engine/start_game_engine.js_header" >> compiled_music_dodge_and_gather.js
+
+#definitions to function "references", that which also define variables as they are ran.
+cat "./start_game_engine/start_game_engine.js_definition" >> compiled_music_dodge_and_gather.js
