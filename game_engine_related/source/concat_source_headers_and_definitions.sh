e426be981273e2e2dfefdeceee2a8bc63ca8a12e
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-05-30 15:40:40 -0700

    Update concat_source_headers_and_definitions.sh

--- game_engine_related/source/concat_source_headers_and_definitions.sh
+++ game_engine_related/source/concat_source_headers_and_definitions.sh
@@ -5,6 +5,9 @@ rm ./compiled_music_dodge_and_gather.js
 #headers (non defined variables and function "references")
 cat "./start_game_engine/start_game_engine.js_header" >> compiled_music_dodge_and_gather.js
 cat "./tick_game_conditions/tick_game_condition.js_header" >> compiled_music_dodge_and_gather.js
+cat "./xmlhttprequest/single_worker_xmlhttprequest.js_header" >> compiled_music_dodge_and_gather.js
+cat "./prerender_assistant/combine_vertex_and_rgba_color.js_header"  >> compiled_music_dodge_and_gather.js
+cat "./prerender_assistant/combine_vertex_and_texture_coordinates.js_header" >> compiled_music_dodge_and_gather.js
 #scene
 cat "./scene/scene.js_header" >> compiled_music_dodge_and_gather.js
 #scene major one
@@ -18,6 +21,9 @@ cat "./scene/major_one/minor_one/prestages/scene_major_one_minor_one_prestages.j
 #definitions to function "references", that which also define variables as they are ran.
 cat "./start_game_engine/start_game_engine.js_definition" >> compiled_music_dodge_and_gather.js
 cat "./tick_game_conditions/tick_game_condition.js_definition" >> compiled_music_dodge_and_gather.js
+cat "./xmlhttprequest/single_worker_xmlhttprequest.js_definition" >> compiled_music_dodge_and_gather.js
+cat "./prerender_assistant/combine_vertex_and_rgba_color.js_definition"  >> compiled_music_dodge_and_gather.js
+cat "./prerender_assistant/combine_vertex_and_texture_coordinates.js_definition" >> compiled_music_dodge_and_gather.js
 #scenes
 #scene major one
 cat "./scene/major_one/scene_major_one.js_definition" >> compiled_music_dodge_and_gather.js
