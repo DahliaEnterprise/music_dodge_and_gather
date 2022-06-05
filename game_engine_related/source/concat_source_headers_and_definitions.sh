e67649b9c68f1121f41ce564e6f1135f35ed24e2
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-06-05 15:47:14 -0700

    Update concat_source_headers_and_definitions.sh

--- game_engine_related/source/concat_source_headers_and_definitions.sh
+++ game_engine_related/source/concat_source_headers_and_definitions.sh
@@ -8,6 +8,8 @@ cat "./tick_game_conditions/tick_game_condition.js_header" >> compiled_music_dod
 cat "./xmlhttprequest/single_worker_xmlhttprequest.js_header" >> compiled_music_dodge_and_gather.js
 cat "./prerender_assistant/combine_vertex_and_rgba_color.js_header"  >> compiled_music_dodge_and_gather.js
 cat "./prerender_assistant/combine_vertex_and_texture_coordinates.js_header" >> compiled_music_dodge_and_gather.js
+#scale
+cat "./scale/scale_by_magnitude_xy.js_header" >> compiled_music_dodge_and_gather.js
 #scene
 cat "./scene/scene.js_header" >> compiled_music_dodge_and_gather.js
 #scene major one
@@ -24,6 +26,8 @@ cat "./tick_game_conditions/tick_game_condition.js_definition" >> compiled_music
 cat "./xmlhttprequest/single_worker_xmlhttprequest.js_definition" >> compiled_music_dodge_and_gather.js
 cat "./prerender_assistant/combine_vertex_and_rgba_color.js_definition"  >> compiled_music_dodge_and_gather.js
 cat "./prerender_assistant/combine_vertex_and_texture_coordinates.js_definition" >> compiled_music_dodge_and_gather.js
+#scale
+cat "./scale/scale_by_magnitude_xy.js_definition" >> compiled_music_dodge_and_gather.js
 #scenes
 #scene major one
 cat "./scene/major_one/scene_major_one.js_definition" >> compiled_music_dodge_and_gather.js
